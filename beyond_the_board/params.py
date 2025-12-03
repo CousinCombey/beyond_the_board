858e99f18de68b4411abaa09d14977e1c70e327a
Paul Marty <paul_marty@outlook.com>
2025-12-03 17:55:01 +0100

    simulate_game func to review

--- beyond_the_board/params.py
+++ beyond_the_board/params.py
@@ -1,11 +1,11 @@
 import os
 import numpy as np
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # Load .env file from project root
 env_path = Path(__file__).parent.parent / '.env'
-load_dotenv(dotenv_path=env_path)
+# load_dotenv(dotenv_path=env_path)
 
 ##################  VARIABLES  ##################
 
