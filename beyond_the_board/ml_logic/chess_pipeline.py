270fcf2bc9ca9be2041d8d83db5b6ed5b0c13e3b
Julien Debote <julien.debote34@gmail.om>
2025-12-05 12:26:22 +0100

    model entrainé sur un sample 200k avec blanc et noir et poid enregistré

--- beyond_the_board/ml_logic/chess_pipeline.py
+++ beyond_the_board/ml_logic/chess_pipeline.py
@@ -6,14 +6,16 @@ from keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
 
 from beyond_the_board.models.cnn_paul import fen_to_tensor_8_8_12
-from beyond_the_board.tensor.new_df import create_new_df_white, drop_columns, create_new_df_black
+from beyond_the_board.tensor.new_df_merged import *
 
-from beyond_the_board.models.cnn_john import *
+from beyond_the_board.models.cnn_john_full import *
 from beyond_the_board.params import *
 
+def complete_pipeline(url:str):
+    """pour l'utiliser : trained_model, history = complete_pipeline('https://storage.googleapis.com/beyond_the_board/Final%20Dataset/games_merged_clean.csv')"""
+
 def data_and_pross(url:str):
-    """ ecrire : dfw, dfb = data_and_pross(DATA_WITH_PGN) pour recuperer vos
-    deux data set blanc et noir"""
+    """ rend un df sans le meta data mais bien trié"""
 
     def read_csv(url):
         """Fonction qui permet de téléchargé le csv"""
@@ -25,30 +27,37 @@ def data_and_pross(url:str):
     def create_input1(df):
 
         """Fonction qui permet de crée a partir du dataset de base
-        un dataset ou c'est au blanc de jouer, et un ou c'est les noirs"""
+        un dataset nettoyer avec fen blanc et fen noir et toutes les
+        nouvelles colonnes"""
 
-        dfw = create_new_df_white(df)
-        dfb = create_new_df_black(df)
+        df = create_new_df_all_merged(df)
 
-        return dfw, dfb
 
-    def sample_df(dfw, dfb, n_samples=None):
+        return df
+
+    def sample_df(df):
 
         """Fonction qui sample 200 000 lignes du data set de base pour
         entrainer le model"""
-        if n_samples is not None:
-            dfw = dfw.sample(n_samples)
-            dfb = dfb.sample(n_samples)
 
-            return dfw, dfb
+        df = df.sample(200000)
+
+
+        return df
 
     df = read_csv(url)
-    dfw, dfb = create_input1(df)
-    dfw, dfb = sample_df(dfw, dfb, n_samples=200000)
+    df = create_input1(df)
+    df = sample_df(df)
 
-    return dfw, dfb
+    return df
 
-def pipeline():
+def pipeline(df):
     """Fonction qui permet tout automatisé et de prédire le prochain coup"""
+    meta_data = drop_columns_merged(df)
+    meta_data = meta_data.columns.to_list()
 
-    pass
+    trained_model, history = complete_workflow_example_full(df, "FEN", "Stockfish", meta_data)
+
+    return trained_model, history
+
+    return trained_model, history
