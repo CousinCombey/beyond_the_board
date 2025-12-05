4224da7a15a539296e568a9781f220d8634eef4c
John Kouto <60616063+CousinCombey@users.noreply.github.com>
2025-12-05 18:03:18 +0100

    End of Day Push

--- beyond_the_board/models/load_upload_model.py
+++ beyond_the_board/models/load_upload_model.py
@@ -1,4 +1,5 @@
-from gcloud import storage
+from google.cloud import storage
+import tensorflow as tf
 
 def upload_model(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
@@ -8,6 +9,10 @@ def upload_model(bucket_name, source_file_name, destination_blob_name):
     # source_file_name = "local/path/to/file"
     # The ID of your GCS object
     # destination_blob_name = "storage-object-name"
+    # client = storage.Client()
+    # bucket = client.bucket(BUCKET_NAME)
+    # blob = bucket.blob(f"models/{model_filename}")
+    blob.upload_from_filename(model_path)
 
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
@@ -21,8 +26,34 @@ def upload_model(bucket_name, source_file_name, destination_blob_name):
     # generation-match precondition using its generation number.
     generation_match_precondition = 0
 
-    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
+    blob.upload_from_filename(source_file_name)
 
     print(
         f"File {source_file_name} uploaded to {destination_blob_name}."
     )
+
+
+def load_model(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a model file from the bucket and loads it as a TensorFlow model."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The ID of your GCS object
+    # source_blob_name = "storage-object-name"
+    # The path to save the file locally
+    # destination_file_name = "local/path/to/file"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    print("bucket correct")
+    blob = bucket.blob(source_blob_name)
+
+    # Download the model file
+    blob.download_to_filename(destination_file_name)
+
+    print(
+        f"Model {source_blob_name} downloaded to {destination_file_name}."
+    )
+
+    # Load and return the TensorFlow model
+    model = tf.keras.models.load_model(destination_file_name)
+    return model
